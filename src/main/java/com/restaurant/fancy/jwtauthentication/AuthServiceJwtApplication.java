82ceb9e10d1d8e13b10c12c7f7063d1070511ae3
johnbabujames <babu.sjc@gmail.com>
2019-06-08 23:15:01 -0400

    first commit

---
+++ src/main/java/com/restaurant/fancy/jwtauthentication/AuthServiceJwtApplication.java
@@ -0,0 +1,12 @@
+package com.restaurant.fancy.jwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceJwtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/restaurant/fancy/jwtauthentication/AuthServiceJwtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restaurant.fancy.jwtauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthServiceJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
